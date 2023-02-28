de5a02b54a7dfaaebad358b1d43c41d543ea3087
Jesus Gonzalez <chuchogonzo26@Mac-and-Chuy.local>
2023-02-27 20:29:06 -0800

    save data

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -2,9 +2,10 @@ package edu.ucsd.cse110.lab4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -12,10 +13,41 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        loadProfile();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+
+        saveProfile();
     }
 
     public void onGoBackClicked(View view) {
-        // Intent main_activity = new Intent(this, MainActivity.class);
-        this.finish();
+        finish();
+    }
+
+    public void loadProfile(){
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+
+        TextView nameView = findViewById(R.id.name_textview);
+        TextView statusView = findViewById(R.id.status_textview);
+
+        nameView.setText(preferences.getString("name", ""));
+        statusView.setText(preferences.getString("status", ""));
+
+    }
+
+    public void saveProfile(){
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        TextView nameView = findViewById(R.id.name_textview);
+        TextView statusView = findViewById(R.id.status_textview);
+        editor.putString("name", nameView.getText().toString());
+        editor.putString("status", statusView.getText().toString());
+
+        editor.apply();
     }
 }
